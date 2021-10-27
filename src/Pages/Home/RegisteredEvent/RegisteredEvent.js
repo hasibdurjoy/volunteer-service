4144ea6dfce4f2a80ff38e01fbc4d16af05d3921
hasibdurjoy <hrd2571@gmail.com>
2021-10-27 23:28:05 +0600

    update

--- src/Pages/Home/RegisteredEvent/RegisteredEvent.js
+++ src/Pages/Home/RegisteredEvent/RegisteredEvent.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Col } from 'react-bootstrap';
 
 const RegisteredEvent = ({ event }) => {
+
+    const [volunteers, setVolunteers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/volunteers')
+            .then(res => res.json())
+            .then(data => setVolunteers(data))
+    }, []);
+
+    const handleDeleteUser = (id) => {
+        const proceed = window.confirm('Are you sure, you want to delete?');
+        if (proceed) {
+            const url = `http://localhost:5000/volunteers/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remainingVolunteers = volunteers.filter(volunteer => volunteer._id !== id);
+                        setVolunteers(remainingVolunteers);
+                        // window.reload();
+                        window.location.reload();
+                    }
+                });
+        }
+    }
+
     return (
         <Col>
             <Card className=" h-100 ">
@@ -12,7 +40,7 @@ const RegisteredEvent = ({ event }) => {
                 <Card.Text>
                     <p>Register Date : {event.date}</p>
                     <p>Register Time : {event.time}</p>
-                    <button className="btn btn-danger mb-4">Cancel</button>
+                    <button onClick={() => { handleDeleteUser(event._id) }} className="btn btn-danger mb-4">Cancel</button>
                 </Card.Text>
             </Card>
         </Col>
